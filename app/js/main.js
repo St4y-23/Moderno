04d3fe1ee2ad06bb03bae01f90252f48bc590dc5
St4y-23 <ghost19011@gmail.com>
2019-10-05 17:54:30 +0400

    adaptive header & footer

--- app/js/main.js
+++ app/js/main.js
@@ -33,7 +33,22 @@ $(function(){
       $('.icon-th-large').addClass('active');
       $('.icon-th-list').removeClass('active');
     });
-    
+
+// При клике будет появляться 'menu__list'
+    $('.menu__btn').on('click', function(){
+      $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function(){
+      $('.header__box').toggleClass('active');
+    });
+
+
+
+
+
+
+
 
     var mixer = mixitup('.products__inner-box');
 
