e0de418f07879ed6e858a9fb450830ea69e6f2f0
St4y-23 <ghost19011@gmail.com>
2019-10-26 20:57:08 +0400

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,37 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1900,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 1441,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 801,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+        ]
+      });
+      $(".slider-top__slider").slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
       });
 
       $(".js-range-slider").ionRangeSlider({
